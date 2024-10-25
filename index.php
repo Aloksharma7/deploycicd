5c8359483e1c25e33dcc6c174e78aaa856eae748
Alok sharma <aloksharma7834@gmail.com>
2024-10-25 20:47:30 +0545

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New asite </title>
+</head>
+<body>
+    <h1>Welcome to this site which is deployed by using github ci/cd pipeline</h1>
+</body>
+</html>
